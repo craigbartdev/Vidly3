5b7db28dffea4e362b7fe059a1c80a5ae51affb8
wavydomez <craigbartjr@yahoo.com>
2019-12-02 23:05:07 -0800

    Section 1 completed

---
+++ Vidly3/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly3.Models; //require models
+
+namespace Vidly3.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie); //need view called Random that references movie
+        }
+    }
+}
